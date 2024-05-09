4446c8eea219194bff344165edc0625adeb8444e
Nikita <nikitasomusev@gmail.com>
2024-05-09 18:22:31 +0300

    added fillable and casts in model

--- modules/Order/Models/Order.php
+++ modules/Order/Models/Order.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'total_in_cents',
+        'payment_gateway',
+        'payment_id',
+    ];
+
+    protected $casts = [
+        'user_id' => 'integer',
+        'total_in_cents' => 'integer',
+    ];
 }
